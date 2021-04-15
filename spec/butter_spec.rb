b31871c180d91403d78bb76e224445b16df4cee0
Michael Duncan <mwduncan2018@gmail.com>
2021-04-15 19:12:03 -0400

    added 2 tests

--- spec/butter_spec.rb
+++ spec/butter_spec.rb
@@ -2,9 +2,23 @@ require "butter"
 
 RSpec.describe "Butter Feature:" do
 
+    context "Given butter has a price of 10 dollars" do
+        it "Then the butter is not expensive" do
+            butter = Butter.new("Butter A", 10)
+            expect(butter.is_expensive?).to eq(false)
+        end
+    end
+
     context "Given butter has a price of 15 dollars" do
-        butter = Butter.new("Butter A", 15)
+        it "Then the butter is not expensive" do
+            butter = Butter.new("Butter A", 15)
+            expect(butter.is_expensive?).to eq(false)
+        end
+    end
+
+    context "Given butter has a price of 20 dollars" do
         it "Then the butter is expensive" do
+            butter = Butter.new("Butter A", 20)
             expect(butter.is_expensive?).to eq(true)
         end
     end
